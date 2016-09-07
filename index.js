68414f2b7d559dd98611b23a914cc8eb6a0307f5
Jérémy M <jeremy.magrin@epitech.eu>
2016-09-07 11:46:22 +0200

    Source Map index

--- index.js
+++ index.js
@@ -2,20 +2,45 @@
 
 import StackTrace from 'stacktrace-js';
 import { Crashlytics } from 'react-native-fabric';
+import SourceMap from 'source-map';
 
-function init() {
+function nomap(row){
+  return {};
+}
+
+function init(smap) {
   if (__DEV__) {
     // Don't send exceptions from __DEV__, it's way too noisy!
     // Live reloading and hot reloading in particular lead to tons of noise...
     return;
   }
 
+  let mapper = nomap;
+  if(smap){
+    const mapConsumer = new SourceMap.SourceMapConsumer(smap);
+    mapper = (row)=>{
+      const loc = mapConsumer.originalPositionFor({
+                                  line: row.lineNumber,
+                                  column: row.columnNumber,
+                              });
+      return loc;
+    }
+  }
+
   var originalHandler = global.ErrorUtils.getGlobalHandler();
-  function errorHandler(e, isFatal) {
-    StackTrace.fromError(e, {offline: true}).then((x)=>Crashlytics.recordCustomExceptionName(e.message, e.message, x));
+  function errorHandler(e) {
+    StackTrace.fromError(e).then((x)=>Crashlytics.recordCustomExceptionName(e.message, e.message, x.map(row=>{
+      const loc = mapper(row);
+      return {
+        fileName: loc.source || e.message,
+        columnNumber: loc.column || row.columnNumber,
+        lineNumber: loc.line || row.lineNumber,
+        functionName: `${loc.name}@${loc.source} ${loc.line}:${loc.column}` //next best thing without a consistent function name
+      };
+    })));
     // And then re-throw the exception with the original handler
     if (originalHandler) {
-      originalHandler(e, isFatal);
+      originalHandler(e);
     }
   }
   global.ErrorUtils.setGlobalHandler(errorHandler);
